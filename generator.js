a4e2f5aadecb1d142a06546a79f1f31fe190ca33
Roman Kubiv <romankubiv101@gmail.com>
2021-02-01 22:07:46 +0000

    Update progress

--- generator.js
+++ generator.js
@@ -1,170 +1,176 @@
+'use strict';
 const fs = require('fs');
-const childProcess = require('child_process')
+const childProcess = require('child_process');
+
 const metadata = require('music-metadata');
-const defaultArgs = require('./args')
 
 module.exports = (args) => {
-    // sets default arguments if none provided
-    args = defaultArgs(args);
 
-    let fetchPlaylistFileName = (path) => new Promise((resolve,reject) => {
+    let fetchPlaylistFileName = (path) => {
 
-        fs.readdir(path,(error,files) => {
-            if (error){
-                reject(error)
-            
-            } else {
-    
-                // filter non playlist files
-                files = files.filter((file) => {
-                    return file.endsWith(".m3u")
-                })
-                
-                // if a file exists
-                if (files[0]){
-                    resolve(files)
-    
-                } else {
-                    reject(new Error("No Playlist File Found"))
-                }
-            }
-    
-        })
-    })  
-    
-    let fetchAudioFileNames = (playlistFileUrl) => new Promise((resolve,reject) => {
-        
-        fs.readFile(playlistFileUrl,(error,data) => {
-            if (!error){
-    
-                let nameList = data.toString().split("\n").filter(val => val != "")
-                //let y = nameList.map(line => {return line.slice(0,-1)})
-                resolve(nameList)
-    
-            } else {
-                reject(error)
-            }
-        })
-    }) 
-    
-    let getTotalAudioDuration = (files) => new Promise((resolve,reject) =>{
-    //TODO maybe refactor
-        let allPromises = files.map((url) => {
-            return getSinglePlaytime(url)
-        })
-     
-        Promise.all(allPromises)
-            .then((results) => {
-             
-            // get sum of play time rounded to nearest second
-            resolve(Math.ceil( results.reduce((accum,val) => {return accum + val})))
-            
-        }).catch(error => {
-            reject(error)
-        })
-        
-    
-    })
-    
-    let getSinglePlaytime = (url) => new Promise((resolve,reject) => {
-    
-        metadata.parseFile(url)
-            .then( metadata => {
-            resolve(metadata.format.duration);
-      
-            }).catch( err => {
-            reject(err.message);
+        let files = fs.readdirSync(path);
+
+        if (!files[0]) {
+            let err = new Error(`No files Found`);
+            err.path = path;
+            throw err;
+        }
+
+        if (files.some(x => x.endsWith('.mp4'))) {
+            let err = new Error("directory contains a video, stoping to prevent overwriting")
+            err.path = path;
+            throw err;
+        }
+
+        // filter non playlist files
+        files = files.filter((file) => file.endsWith(".m3u"));
+
+        // if a file exists
+        if (files[0]) {
+            return files[0];
+
+        } else {
+            let err = new Error(`No Playlist File Found`);
+            err.path = path;
+            throw err;
+        }
+    }
+
+    let fetchAudioFileNames = (playlistFileURL) => {
+        let file = fs.readFileSync(playlistFileURL);
+
+        // split by line and remove \r chars
+        return file.toString().split(/[\n\r\s]/g).filter(val => val);
+    }
+
+    let getVideoDuration = (audioFiles) => new Promise((resolve, reject) => {
+        Promise.all(audioFiles.map(getSinglePlaytime))
+            .then(results => resolve([Math.ceil(results.reduce((accum, val) => accum + val)), undefined])) // sum play time rounded to nearest second
+            .catch(err => {
+                console.log(err);
+                resolve([-1, err]);
             });
     })
-    
-    let getNameRoot = (directoryPath) => {
+
+    let getSinglePlaytime = (URL) => new Promise((resolve, reject) => {
+        metadata.parseFile(URL)
+            .then(metadata => resolve(metadata.format.duration))
+            .catch(reject);
+    })
+
+    let getFolderName = (directoryPath) => {
         let directoryName = directoryPath.split("\\").slice(-1)[0]
-        return directoryPath + "\\" + directoryName.slice(0,-4)
+        return directoryPath + "\\" + directoryName.slice(0, -4)
     }
-     
-    let createVideo = (fileList,playtime,imagePath,outputFilename) => new Promise((resolve,reject) =>{
-    
-        let complexFilterArguments = fileList.map((val,index) => {return `[${index+1}:a]`}).join('')
-    
-        childProcess.exec(`ffmpeg -loop 1 -framerate 1 \
-        -i "${imagePath}" \
-        -i ${fileList.map(line => `"${line}"`).join(" -i ")} \
-        -filter_complex "[0]scale='iw-mod(iw,2)':'ih-mod(ih,2)',format=yuv420p[v];${complexFilterArguments}concat=n=${fileList.length}:v=0:a=1[a]" \
-        -map "[v]" -r 15 -map "[a]" \
-        -tune stillimage -t ${playtime} -movflags +faststart "${outputFilename}" `
-        ,(error,stdout,stderr) => {
-            console.log(stdout)
-            
-            if (error){
-                reject(error)
-            
-            } else {
-                resolve(outputFilename)
-            }
-    
+
+    let secodnsToHMSTime = d => {
+        var h = Math.floor(d / 3600);
+        var m = Math.floor(d % 3600 / 60);
+        var s = Math.floor(d % 3600 % 60);
+        return `${h}h:${m}m:${s}s`
+    }
+
+    let showVideoDetails = (outputFileName, playtime, fileList) => {
+        let infoString = `begining to generate video ${outputFileName} | duration ${secodnsToHMSTime(playtime)}\nfrom file(s):`.toString();
+        console.log(new Array(infoString.length + 1).join("-"))
+        console.log(infoString);
+        fileList.forEach(file => console.log(file));
+    }
+
+    let generateVideo = (fileList, imagePath, outputFileName, playtime) => new Promise(async (resolve, reject) => {
+
+        showVideoDetails(outputFileName, playtime, fileList);
+        let complexFilterArguments = fileList.map((val, index) => {
+            return `[${index+1}:a]`
+        }).join('')
+
+        let proc = childProcess.spawn("ffmpeg", ['-loop', '1', '-framerate', '1',
+            '-i', imagePath,
+            ...[].concat.apply([], fileList.map((file, ind) => ["-i", file])),
+            '-filter_complex', `[0]scale='iw-mod(iw,2)':'ih-mod(ih,2)',format=yuv420p[v];${complexFilterArguments}concat=n=${fileList.length}:v=0:a=1[a]`,
+            '-map', '[v]', '-r', '15', '-map', '[a]',
+            '-tune', 'stillimage', '-t', playtime, '-movflags', '+faststart', outputFileName,
+        ])
+
+        proc.on("exit", (code, signal) => {
+            if (code === 0) resolve(outputFileName)
+            else reject(signal);
         })
     })
-    
-    var generate = (directoryUrl, foregroundImgUrl) => new Promise(async (resolve,reject) => {
-    
-    
-        let playlistFileName = await fetchPlaylistFileName(directoryUrl);
-        console.log("playlist file : ",playlistFileName)
-    
-        let audioFiles = await fetchAudioFileNames( directoryUrl + "\\" + playlistFileName)
-        console.log("audio files : ",audioFiles)
-    
-        // prepend paths to filenames
-        audioFiles = audioFiles.map(fileUrl => {
-            return directoryUrl+"\\"+fileUrl
-        })
-    
-        let totalAudioDuration = await getTotalAudioDuration(audioFiles)
-        console.log("calculated total playtime ", totalAudioDuration)
-    
-        let outputFilename = getNameRoot(directoryUrl) + "_video.mp4"
-        console.log("generating video", outputFilename)
-            
-        let generatedPath = await createVideo(audioFiles, totalAudioDuration, foregroundImgUrl, outputFilename)
-        resolve(generatedPath) 
-    })  
-    
-    let applyToSubfoldersOf = async (dir,func) => {
-        console.log("function is ", func)
+
+    var startVideoGeneration = (sourceURL, foregroundImgURL) => new Promise(async (resolve, reject) => {
+
+        let playlistFileName;
+        try {
+            playlistFileName = fetchPlaylistFileName(sourceURL);
+
+        } catch (err) {
+            return resolve([err.message, err.path]);
+        }
+
+        try {
+            console.log('fetching files from', sourceURL + "\\" + playlistFileName);
+            let audioFileNames = fetchAudioFileNames(sourceURL + "\\" + playlistFileName);
+
+
+            // prepend paths to filenames
+            let audioFilePaths = audioFileNames.map(fileURL => sourceURL + "\\" + fileURL);
+
+            let [playtime, err] = await getVideoDuration(audioFilePaths);
+
+            let outputFileName = getFolderName(sourceURL) + "_video.mp4";
+
+            if (playtime === -1) {
+                console.log("WARNING", outputFileName, "failed to start, it may be missing files, the playlist file is incorrect or some audio files are corrupt");
+                reject(err);
+                reject(new Error("Failed to fetch playtime"));
+
+            } else {
+                generateVideo(audioFilePaths, foregroundImgURL, outputFileName, playtime).then(() => {
+                    console.log("Finished", outputFileName);
+                    resolve();
+                });
+            }
+
+        } catch (err) {
+            reject(err);
+        }
+    })
+
+    let applyToSubfoldersOf = async (dir, func) => {
         func(dir)
-    
+
         fs.readdirSync(dir)
-            .filter((name) => {return name.indexOf(".") == -1 })
-            .forEach(folderName => {
-                applyToSubfoldersOf(dir + "\\" + folderName,func)
+            .filter((name) => {
+                return name.indexOf(".") === -1
             })
-            
+            .forEach(folderName => {
+                applyToSubfoldersOf(dir + "\\" + folderName, func)
+            })
     }
-    
+
     //TODO change error code handling
     //TODO add options for output file picking
     //TODO change parameter passing
-    
-    var generateForSubFolders = (directoryUrl, foregroundImgUrl) => new Promise((resolve,reject) => {
-    
+
+    var generateForSubFolders = (directoryURL, foregroundImgURL) => new Promise((resolve, reject) => {
+
         let allPromises = []
-    
-        applyToSubfoldersOf(directoryUrl,() => {
+
+        applyToSubfoldersOf(directoryURL, (dir) => {
             allPromises.push(
-                generate(directoryUrl,foregroundImgUrl)        )
+                startVideoGeneration(dir, foregroundImgURL))
         })
-    
+
         Promise.allSettled(allPromises)
             .then(values => {
-                console.log(values)
-                resolve()
+                resolve(values.filter(val => val.value))
             })
-    
     })
 
 
     return {
-        singleDirectory : generate,
-        recursiveDirectory:  generateForSubFolders
+        singleDirectory: startVideoGeneration,
+        recursiveDirectory: generateForSubFolders
     }
 }
