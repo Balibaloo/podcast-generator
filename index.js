95e2c96f6a985a9fcc9da4b2b3295371edd246f7
Roman Kubiv <sc20rk@leeds.ac.uk>
2021-04-07 14:03:32 +0100

    temp

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ const generator = require("./podcast-generator")
 let imagePath = "C:\\Repos\\podcast-generator\\MA.jpg";
 // let imagePath = "C:\\Pictures\\Ma-Vinil.png"
 // let searchDirectory = "C:\\Repos\\podcast-generator\\testDir"
-let searchDirectory = "C:\\Repos\\podcast-generator\\tempFiles\\X Luc";
-// let searchDirectory = "D:\\Confer numérisées - MP3 pour baladeur\\1990\\90_01_17_lausanne_MP3";
+let searchDirectory = "C:\\Repos\\podcast-generator\\tempFiles";
+// let searchDirectory = "C:\\Repos\\podcast-generator\\tempFiles\\Done\\Luc done\\93_05_08_giez_MP3";
 let outputDirectory = "";
 
 
 generator({}).recursiveDirectory(searchDirectory, imagePath, outputDirectory)
     .then((results) => {
-        console.log("results = ", results)
+        console.log("results = ", results.filter(result => result[0] === "failed"))
 
     }).catch((error) => {
         console.log(error)
